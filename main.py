b9396b26984b88d4348a7336e5dee6e6bd469660
Priyakshi Goswami <priyakshig88@gmail.com>
2024-04-15 05:38:23 +0200

    t

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ else:
 
 
 class Arguments(Tap):
-    dataset: str = 'flickr'
+    dataset: str = 'cora'
 
     sampling_hops: int = 2
     num_samples: int = 32
@@ -88,7 +88,7 @@ def train(args: Arguments):
         num_indicators = 0
 
     if args.model_type == 'gcn':
-        gcn_c = ResGCN(data.num_features, hidden_dims=[args.hidden_dim, num_classes], dropout=args.dropout).to(device)
+        gcn_c = GCN(data.num_features, hidden_dims=[args.hidden_dim, num_classes], dropout=args.dropout).to(device)
         # GCN model for GFlotNet sampling
         gcn_gf = GCN(data.num_features + num_indicators,
                       hidden_dims=[args.hidden_dim, 1]).to(device)
