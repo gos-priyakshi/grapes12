0b8770061fd6a8a0aee6d448566b276afbd8d353
Priyakshi Goswami <priyakshig88@gmail.com>
2024-07-11 15:32:54 +0200

    fbar

--- modules/plot.py
+++ modules/plot.py
@@ -4,12 +4,14 @@ import numpy as np
 # Sample data
 x_values = ['2', '4', '8', '16', '32', '64', '-1']
 y_values = [0.23, 0.109, 0.018, 0.001, 0.000012, 0.0000000022, 0.0000000008]
-
+y_2 = [1.03, 0.45, 0.15, 0.01, 0.00005, 0.000000001, 0.0000000003]
 # apply log scale to y_values and x values
 y_values = np.log2(y_values)
+y_2 = np.log2(y_2)
 
 # Plotting the curve and save it
-plt.plot(x_values, y_values, marker='o')  # 'marker' is optional, it adds circles at each point
+plt.plot(x_values, y_values, label='Dirichlet Energy 1')
+plt.plot(x_values, y_2, label='Dirichlet Energy 2')
 plt.title('Simple Curve Plot')
 plt.xlabel('layers')
 plt.ylabel('Y-axis')
