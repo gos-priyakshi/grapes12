fc549e9ce860d11f11441227f67ba87a34444f10
Priyakshi Goswami <priyakshig88@gmail.com>
2024-07-29 02:34:19 +0200

    fg

--- modules/gcn123.py
+++ modules/gcn123.py
@@ -67,7 +67,7 @@ class GCN(nn.Module):
     def get_intermediate_outputs(self, x: torch.Tensor, adjacency: Union[torch.Tensor, List[torch.Tensor]]) -> List[torch.Tensor]:
         intermediate_outputs = []
 
-        target_layers = [2, 4, 8, 16, 32, 64, 128]
+        target_layers = [2, 4, 8, 16, 32, 64]
         
         for i, layer in enumerate(self.gcn_layers[:-1]):
             adj = adjacency[-(i + 1)] if isinstance(adjacency, list) else adjacency
@@ -306,7 +306,7 @@ class GCNII(nn.Module):
         x = h0 
 
         intermediate_outputs = []
-        target_layers = [2, 4, 8, 16, 32, 64, 128]
+        target_layers = [2, 4, 8, 16, 32, 64]
 
         for i, layer in enumerate(self.gcn_layers[:-1]):
             adj = adjacency[-(i + 1)] if isinstance(adjacency, list) else adjacency
