cee7ec2eb98d54816e2a42ee738b096ec3b432b2
Priyakshi Goswami <priyakshig88@gmail.com>
2024-08-05 21:10:59 +0200

    44

--- modules/plot2.py
+++ modules/plot2.py
@@ -3,14 +3,14 @@ import numpy as np
 
 # Sample data
 x_values = ['2', '4', '8', '16', '32', '64', '128']
-y_gcn_s = [0.701, 0.663, 0.551, 0.386, 0.303, 0.231, 0.181]
-y_gcn_f = [0.421, 0.7, 0.735, 0.704, 0.714, 0.3, 0.16]
-y_res_s = [0.716, 0.688, 0.711, 0.72, 0.737, 0.742, 0.749]
-y_res_f = [0.694, 0.715, 0.71, 0.718, 0.717, 0.686, 0.173]
-y_gcn2_s = [0.705, 0.707, 0.738, 0.728, 0.57, 0.222, 0.29]
-y_gcn2_f = [0.381, 0.371, 0.337, 0.449, 0.483, 0.607, 0.597]
-y_gat_s = [0.737, 0.768, 0.75, 0.577, 0.441, 0.285, 0.181]
-y_gat_f = [0.421, 0.442, 0.369, 0.409, 0.222, 0.231, 0.181]
+y_gcn_s = [0.86, 0.845, 0.828, 0.836, 0.869, 0.413, 0.413]
+y_gcn_f = [0.866, 0.838, 0.84, 0.836, 0.709, 0.407, 0.407]
+y_res_s = [0.857, 0.834, 0.85, 0.844, 0.88, 0.873, 0.877]
+y_res_f = [0.856, 0.847, 0.838, 0.843, 0.851, 0.859, 0.18]
+y_gcn2_s = [0.876, 0.871, 0.881, 0.873, 0.888, 0.884, 0.407]
+y_gcn2_f = [0.85, 0.847, 0.833, 0.828, 0.789, 0.777, 0.413]
+y_gat_s = [0.87, 0.856, 0.841, 0.839, 0.838, 0.407, 0.407]
+y_gat_f = [0.847, 0.848, 0.832, 0.838, 0.796, 0.407, 0.407]
 
 # Plotting the first set of curves (GCN and GAT)
 plt.figure(figsize=(12, 5))
@@ -20,7 +20,7 @@ plt.plot(x_values, y_gcn_s, label='GCN-grapes', linewidth=2)
 plt.plot(x_values, y_gcn_f, label='GCN', linewidth=2)
 plt.plot(x_values, y_gat_s, label='GAT-grapes', linewidth=2)
 plt.plot(x_values, y_gat_f, label='GAT', linewidth=2)
-plt.title('Citeseer - GCN and GAT')
+plt.title('Pubmed - GCN and GAT')
 plt.xlabel('Layers')
 plt.ylabel('f1_score')
 plt.legend()  # Add legend
@@ -32,7 +32,7 @@ plt.plot(x_values, y_res_s, label='ResNet-grapes', linewidth=2)
 plt.plot(x_values, y_res_f, label='ResNet', linewidth=2)
 plt.plot(x_values, y_gcn2_s, label='GCN2-grapes', linewidth=2)
 plt.plot(x_values, y_gcn2_f, label='GCN2', linewidth=2)
-plt.title('Citeseer - ResNet and GCN2')
+plt.title('Pubmed - ResNet and GCN2')
 plt.xlabel('Layers')
 plt.ylabel('f1_score')
 plt.legend()  # Add legend
